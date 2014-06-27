42c9310bd59102bc3adc57e4875a5bdbd9639c64
firda <firdha@montazze.com>
2014-06-27 10:29:51 +0700

    Plugin Update

--- app/code/community/Merchantplus/Merchantplus/Model/Observer.php
+++ app/code/community/Merchantplus/Merchantplus/Model/Observer.php
@@ -2,16 +2,9 @@
 /**
  * NOTICE OF LICENSE
  *
- * This source file is subject to the Open Software License (OSL 3.0)
- * which is available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you are unable to obtain it through the world-wide-web,
- * please send an email to magento@raveinfosys.com
- * so we can send you a copy immediately.
- *
- * @category	Montazze
+ * @category	Merchantplus
  * @package		Merchantplus_Merchantplus
- * @author		Montazze Studio.
+ * @author		Merchantplus.
  * @license		http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Merchantplus_Merchantplus_Model_Observer {
